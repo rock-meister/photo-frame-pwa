4fc9abec3d95c625837996f25ac9d2417a51b481
Ramana Kumar <ramana.r.kumar@gmail.com>
2021-10-06 23:06:51 -0500

    Going Minimal

--- service-worker.js
+++ service-worker.js
@@ -12,26 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'photo-frame-pwa-dc-';// + Date.now();
-var cacheName = 'photo-frame-pwa-c-';// + Date.now();
-var filesToCache = [
-];
+var dataCacheName = 'photo-show-dc-';// + Date.now();
+var cacheName = 'photo-show-c-';// + Date.now();
+var filesToCache = []
+// [
+//   '/apple-touch-icon.png',
+//   '/favicon-16x16.png',
+//   '/favicon-32x32.png',
+//   '/favicon.ico',
+//   '/icon-192x192.png',
+//   '/icon-256x256.png',
+//   '/index.html',
+//   '/manifest.json',
+//   '/mstile-150x150.png',
+// ]
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log(`[ServiceWorker] Caching app shell - ${cacheName}`);
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
         console.log(`[ServiceWorker] key=${key}  c=${cacheName} dc=${dataCacheName}`);
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache\n', key);
@@ -53,8 +63,8 @@ self.addEventListener('activate', function (e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('[Service Worker] Fetch', e.request.url);
+self.addEventListener('fetch', function(e) {
+  //console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
@@ -65,8 +75,8 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
      */
     e.respondWith(
-      caches.open(dataCacheName).then(function (cache) {
-        return fetch(e.request).then(function (response) {
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
           cache.put(e.request.url, response.clone());
           return response;
         });
@@ -79,9 +89,10 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
     e.respondWith(
-      caches.match(e.request).then(function (response) {
+      caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
       })
     );
   }
 });
+
